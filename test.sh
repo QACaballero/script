e8c93135f20d926ca7c651b17f1633ad05e613ed
ALvin Caballero <Qacaballero@tiop.edu.ph>
2022-05-09 06:40:02 +0000

    script

---
+++ test.sh
@@ -0,0 +1,11 @@
+#! bin/bash
+echo "Hello World!"
+name "Alvin Caballero"
+echo "Prepared by : $name"
+CURRERT_DIRECTORY= pwd
+echo "You are in the $CURRENT_DiRECTORY"
+echo -n "The time is"
+date
+echo -n "What is your name?"
+read identity
+echo "Hello $identity"
